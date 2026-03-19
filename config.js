fe1feb5c039ad760fc60fe4c19adca6b3c815590
Igbined <93369396+Igbined@users.noreply.github.com>
2026-03-19 20:54:17 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// Keep this file private / .gitignore it!
+
+const TELEGRAM_BOT_TOKEN = "8690201412:AAFZ13-gmb-2Ax2EXdGlHUhVc19AApHVurc";
+const TELEGRAM_CHAT_ID   = "6487626501";
+
+// You can add more config values later if needed
+// export default { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID };  ← if using modules
